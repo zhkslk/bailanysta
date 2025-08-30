4fbcbe5029edd9720b3a617fb4ed2df547a3ff6e
Zhas <zhassulan.zhaksylyk@gmail.com>
2025-08-30 12:07:49 +0500

    init

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,7 +19,7 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name',
+        'username',
         'email',
         'password',
     ];
@@ -52,7 +52,7 @@ class User extends Authenticatable
      */
     public function initials(): string
     {
-        return Str::of($this->name)
+        return Str::of($this->username)
             ->explode(' ')
             ->take(2)
             ->map(fn ($word) => Str::substr($word, 0, 1))
