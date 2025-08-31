67724731943c19d71a0883a60a497a46aacf5ca3
Zhas <zhassulan.zhaksylyk@gmail.com>
2025-09-01 00:00:46 +0500

    auth logo fix

--- resources/views/components/layouts/auth.blade.php
+++ resources/views/components/layouts/auth.blade.php
@@ -6,12 +6,12 @@
 <body class="min-h-screen bg-neutral-100 antialiased dark:bg-linear-to-b dark:from-neutral-950 dark:to-neutral-900">
     <div class="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
         <div class="flex w-full max-w-md flex-col gap-6">
-            <a href="{{ route('index') }}" class="flex flex-col items-center gap-2 font-medium" wire:navigate>
+            <a href="{{ route('index') }}" class="flex justify-center items-center gap-2 font-medium" wire:navigate>
                 <span class="flex h-9 w-9 items-center justify-center rounded-md">
                     <flux:icon.user-check class="size-9 text-black dark:text-white" />
                 </span>
 
-                <span class="sr-only">{{ config('app.name', 'Laravel') }}</span>
+                <span class="text-2xl">{{ config('app.name', 'Laravel') }}</span>
             </a>
 
             <div class="flex flex-col gap-6">
