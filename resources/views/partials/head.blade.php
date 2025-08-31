ea52da6600a1c7ac4336a3359db32d7a90bb161c
Zhas <zhassulan.zhaksylyk@gmail.com>
2025-08-31 23:42:08 +0500

    title

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,7 +1,7 @@
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-<title>{{ $title ?? config('app.name') }}</title>
+<title>{{ $title ? $title . ' | ' . config('app.name') : config('app.name') }}</title>
 
 <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
